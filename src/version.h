461cd801d57201399504ee28419e20752b6403b8
Remy Oukaour <remy.oukaour@gmail.com>
2017-11-24 13:33:18 -0500

    Version 3.0.5

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PROGRAM_VERSION 3,0,4
+#define PROGRAM_VERSION 3,0,5
 #ifdef _DEBUG
-#define PROGRAM_VERSION_STRING "3.0.4 [DEBUG]"
+#define PROGRAM_VERSION_STRING "3.0.5 [DEBUG]"
 #else
-#define PROGRAM_VERSION_STRING "3.0.4"
+#define PROGRAM_VERSION_STRING "3.0.5"
 #endif
 
 #define PROGRAM_NAME "Polished Map"
