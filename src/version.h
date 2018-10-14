5c5cd3360e077b188a646e5a6b8df79897ba31de
Rangi <remy.oukaour+rangi42@gmail.com>
2018-10-14 17:32:34 -0400

    Version 3.4.0

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PROGRAM_VERSION 3,3,0
+#define PROGRAM_VERSION 3,4,0
 #ifdef _DEBUG
-#define PROGRAM_VERSION_STRING "3.3.0 [DEBUG]"
+#define PROGRAM_VERSION_STRING "3.4.0 [DEBUG]"
 #else
-#define PROGRAM_VERSION_STRING "3.3.0"
+#define PROGRAM_VERSION_STRING "3.4.0"
 #endif
 
 #define PROGRAM_NAME "Polished Map"
