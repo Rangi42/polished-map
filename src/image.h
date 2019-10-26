f3affdc6ada401f6f94efdeb84db8ed7e52a9d76
Rangi <remy.oukaour+rangi42@gmail.com>
2019-10-25 23:42:58 -0400

    Encode tileset images as 8-bit grayscale

--- src/image.h
+++ src/image.h
@@ -17,7 +17,8 @@ public:
 	static Result write_roof_image(const char *f, const Tileset &tileset);
 	static const char *error_message(Result result);
 private:
-	static Result write_image(const char *f, size_t w, size_t h, uchar *buffer, int pd = 1, int d = 3, int ld = 0);
+	static Result write_image(const char *f, size_t w, size_t h, uchar *buffer, bool grayscale = false,
+		int pd = 1, int d = 3, int ld = 0);
 };
 
 #endif
