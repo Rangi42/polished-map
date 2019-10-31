f82a496f8192330830a06c809a002a77e5713c3b
Rangi <remy.oukaour+rangi42@gmail.com>
2019-10-31 18:20:40 -0400

    2.4.2++

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PROGRAM_VERSION 2,4,1
+#define PROGRAM_VERSION 2,4,2
 #ifdef _DEBUG
-#define PROGRAM_VERSION_STRING "2.4.1 [DEBUG]"
+#define PROGRAM_VERSION_STRING "2.4.2 [DEBUG]"
 #else
-#define PROGRAM_VERSION_STRING "2.4.1"
+#define PROGRAM_VERSION_STRING "2.4.2"
 #endif
 
 #ifdef _WIN32
