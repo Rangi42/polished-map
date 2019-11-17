eecc08c47657edb306610591de3e7b31e6ff4288
Rangi <remy.oukaour+rangi42@gmail.com>
2019-11-17 16:03:11 -0500

    Version 4.4.3

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PROGRAM_VERSION 4,4,2
+#define PROGRAM_VERSION 4,4,3
 #ifdef _DEBUG
-#define PROGRAM_VERSION_STRING "4.4.2 [DEBUG]"
+#define PROGRAM_VERSION_STRING "4.4.3 [DEBUG]"
 #else
-#define PROGRAM_VERSION_STRING "4.4.2"
+#define PROGRAM_VERSION_STRING "4.4.3"
 #endif
 
 #ifdef _WIN32
