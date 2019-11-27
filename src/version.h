a42f45efd300e27cd9853436ccc6da9f40be9cf9
Rangi <remy.oukaour+rangi42@gmail.com>
2019-11-27 15:21:09 -0500

    Portable app: use polishedmap.prefs or Polished Map.prefs if it exists in the same directory as polishedmap.exe

--- src/version.h
+++ src/version.h
@@ -8,10 +8,12 @@
 #define PROGRAM_VERSION_STRING "4.4.3"
 #endif
 
+#define PROGRAM_EXE_NAME "polishedmap"
+
 #ifdef _WIN32
-#define PROGRAM_EXE "polishedmap.exe"
+#define PROGRAM_EXE PROGRAM_EXE_NAME ".exe"
 #else
-#define PROGRAM_EXE "polishedmap"
+#define PROGRAM_EXE PROGRAM_EXE_NAME
 #endif
 
 #define PROGRAM_NAME "Polished Map"
