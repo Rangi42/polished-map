cc796d0f63efe97349b3c3615fba20d56bb870f2
Rangi <remy.oukaour+rangi42@gmail.com>
2020-08-23 21:09:58 -0400

    Version 4.5.2

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PROGRAM_VERSION 4,5,1
+#define PROGRAM_VERSION 4,5,2
 #ifdef _DEBUG
-#define PROGRAM_VERSION_STRING "4.5.1 [DEBUG]"
+#define PROGRAM_VERSION_STRING "4.5.2 [DEBUG]"
 #else
-#define PROGRAM_VERSION_STRING "4.5.1"
+#define PROGRAM_VERSION_STRING "4.5.2"
 #endif
 
 #define PROGRAM_EXE_NAME "polishedmap"
