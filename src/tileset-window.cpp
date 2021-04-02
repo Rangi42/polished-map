eedc1aaf5dfc6420de87bc35d64cd29319fee85f
Rangi <remy.oukaour+rangi42@gmail.com>
2021-04-01 21:33:15 -0400

    Pasting graphics into multiple tiles will define palettes for all of them

--- src/tileset-window.cpp
+++ src/tileset-window.cpp
@@ -486,22 +486,19 @@ void Tileset_Window::paste_tile_graphics_cb(Toolbar_Button *tb, Tileset_Window *
 		Fl::paste(*tw->_window, 1, Fl::clipboard_image);
 		return;
 	}
-	if (tw->_selected->palette() == Palette::UNDEFINED) {
-		tw->_selected->palette(Palette::GRAY);
-	}
 	Fl_Image *pasted = (Fl_Image *)Fl::event_clipboard();
 	int ox = tw->_selected->id() % TILES_PER_ROW, oy = tw->_selected->id() / TILES_PER_ROW;
 	int pw = std::min(pasted->w(), (TILES_PER_ROW - ox) * TILE_SIZE);
 	int ph = std::min(pasted->h(), (TILES_PER_COL - oy) * TILE_SIZE);
 	for (int py = 0; py < ph; py++) {
-		int ty = oy + py / TILE_SIZE;
+		int ty = oy + py / TILE_SIZE, tpy = py % TILE_SIZE;
 		if (!Config::allow_256_tiles()) {
 			if (oy < (0x60 / TILES_PER_ROW) && ty >= (0x60 / TILES_PER_ROW)) { ty += 0x20 / TILES_PER_ROW; }
 			if (oy < (0xE0 / TILES_PER_ROW) && ty >= (0xE0 / TILES_PER_ROW)) { ty += 0x20 / TILES_PER_ROW; }
 		}
 		if (ty >= TILES_PER_COL) { continue; }
 		for (int px = 0; px < pw; px++) {
-			int tx = ox + px / TILE_SIZE;
+			int tx = ox + px / TILE_SIZE, tpx = px % TILE_SIZE;
 			if (tx >= TILES_PER_ROW) { continue; }
 			uint8_t id = (uint8_t)(ty * TILES_PER_ROW + tx);
 			Deep_Tile_Button *dtb = tw->_deep_tile_buttons[id];
@@ -509,7 +506,10 @@ void Tileset_Window::paste_tile_graphics_cb(Toolbar_Button *tb, Tileset_Window *
 			uchar c = Color::desaturated((uchar)p[0], (uchar)p[1], (uchar)p[2]);
 			Hue e = Color::mono_hue(c);
 			const uchar *rgb = Color::color(tw->_tileset->palettes(), dtb->palette(), e);
-			dtb->pixel(px % TILE_SIZE, py % TILE_SIZE, e, rgb[0], rgb[1], rgb[2]);
+			dtb->pixel(tpx, tpy, e, rgb[0], rgb[1], rgb[2]);
+			if (tpx == 0 && tpy == 0 && dtb->palette() == Palette::UNDEFINED) {
+				dtb->palette(Palette::GRAY);
+			}
 		}
 	}
 	delete pasted;
