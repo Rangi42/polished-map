6686120c8e3ef6d5970c98aa25a674b5d5fd6554
Rangi <remy.oukaour+rangi42@gmail.com>
2021-08-19 21:21:02 -0400

    Roof tile editor supports Alt+click to swap hues like tileset editor

--- src/roof-window.h
+++ src/roof-window.h
@@ -56,6 +56,7 @@ public:
 	void choose(Swatch *swatch);
 	void flood_fill(Pixel_Button *pb, Hue f, Hue t) const;
 	void substitute_hue(Hue f, Hue t) const;
+	void swap_hues(Hue f, Hue t) const;
 private:
 	static void close_cb(Fl_Widget *w, Roof_Window *rw);
 	static void cancel_cb(Fl_Widget *w, Roof_Window *rw);
