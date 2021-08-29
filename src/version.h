c99c6e68f79436c360217b962f0e6dcd067e6971
Rangi <remy.oukaour+rangi42@gmail.com>
2021-08-29 17:45:23 -0400

    Version 2.7.0++

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PROGRAM_VERSION 2,6,0
+#define PROGRAM_VERSION 2,7,0
 #ifdef _DEBUG
-#define PROGRAM_VERSION_STRING "2.6.0 [DEBUG]"
+#define PROGRAM_VERSION_STRING "2.7.0 [DEBUG]"
 #else
-#define PROGRAM_VERSION_STRING "2.6.0"
+#define PROGRAM_VERSION_STRING "2.7.0"
 #endif
 
 #define PROGRAM_EXE_NAME "polishedmap-plusplus"
