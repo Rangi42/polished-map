0d8ec223d05e5183392eba611d79f5c9ae76e597
Rangi <remy.oukaour+rangi42@gmail.com>
2021-11-01 19:47:03 -0400

    Version 4.7.1

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PROGRAM_VERSION 4,7,0
+#define PROGRAM_VERSION 4,7,1
 #ifdef _DEBUG
-#define PROGRAM_VERSION_STRING "4.7.0 [DEBUG]"
+#define PROGRAM_VERSION_STRING "4.7.1 [DEBUG]"
 #else
-#define PROGRAM_VERSION_STRING "4.7.0"
+#define PROGRAM_VERSION_STRING "4.7.1"
 #endif
 
 #define PROGRAM_EXE_NAME "polishedmap"
